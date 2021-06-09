d6d141882518ad4c278c0f1aba3321a5d468ae67
islomnumanov <islom4ik.numanov@gmail.com>
2021-06-09 13:06:19 +0900

    added new features

--- main.js
+++ main.js
@@ -1,8 +1,38 @@
+const todos = [{
+    text: 'Buy grocery',
+    completed: true
+}, {
+    text: 'Do homework',
+    completed: false
+}, {
+    text: 'Clean the room',
+    completed: true
+}, {
+    text: 'Workout',
+    completed: false
+}, {
+    text: 'Read KGB',
+    completed: true
+}]
 
-const paragraphs = document.querySelectorAll('p')
+const incompleteTasks = todos.filter(function (item, index) {
+    return !item.completed
+})
 
-paragraphs.forEach(function (paragraph) {
-    if (paragraph.textContent.includes('the')) {
-        paragraph.remove()
-    }
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTasks.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function (item) {
+    const p = document.createElement('p')
+    p.textContent = item.text
+    document.querySelector('body').appendChild(p)
+})
+
+document.querySelector('#create-todo').addEventListener('click', function(e) {
+    console.log('I added a new button')
+})
+
+document.querySelector('#filter-todo').addEventListener('input', function (e) {
+    console.log(e.target.value)
 })
