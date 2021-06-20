d803c2aeb57e409d157fb4d64b50677fd6a8a50a
islomnumanov <islom4ik.numanov@gmail.com>
2021-06-20 15:29:12 +0900

    finished rendering the data

--- main.js
+++ main.js
@@ -7,6 +7,7 @@ const todos = [{
 }, {
     text: 'buy food',
     completed: true
+
 }, {text: 'do work',
     completed: false
 }, {
@@ -15,13 +16,16 @@ const todos = [{
 }]
 
 const filters = {
-    searchText: ''
+    searchText: '',
+    hideCompleted: false
 }
 
 
 const renderTodos = function (todos, filters) {
     const filteredTodos = todos.filter(function (todo) {
-        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const searchFilter = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+        const checkboxFilter = !todo.completed || !filters.hideCompleted
+        return searchFilter && checkboxFilter 
     })
     
     const incompleteTodos = filteredTodos.filter(function (todo) {
@@ -46,17 +50,24 @@ renderTodos(todos, filters)
 
 
 // Listen for add todo button click
-document.querySelector('#add-todo').addEventListener('click', function (e) {
-    console.log('Add todo button was clicked')
-})
-
-// Listen for todo text change
-document.querySelector('#todo-text').addEventListener('input', function (e) {
-    console.log(e.target.value)
+document.querySelector('#add-form').addEventListener('submit', function (e) {
+    e.preventDefault()
+    todos.push({
+        text: e.target.elements.addTodo.value,
+        completed: filters.completed
+    })
+    renderTodos(todos, filters)
+    e.target.elements.addTodo.value = ''
 })
 
 //Listen for search todo text change
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
+    renderTodos(todos, filters)
+})
+
+// Listen for the Checkbox
+document.querySelector('#filter-todos').addEventListener('change', function (e) {
+    filters.hideCompleted = e.target.checked
     renderTodos(todos, filters)
 })
