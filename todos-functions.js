53adbf78b6ce95d0c384bcda7346287b40ba9d29
islomnumanov <islom4ik.numanov@gmail.com>
2021-07-06 13:15:55 +0900

    ternary ops

--- todos-functions.js
+++ todos-functions.js
@@ -2,11 +2,13 @@
 const getSavedTodos = () => {
     const todosJSON = localStorage.getItem('todos')
 
-    if (todosJSON !== null) {
-        return JSON.parse(todosJSON)
-    } else {
-        return []
-    }
+    return todosJSON !== null ?  JSON.parse(todosJSON) : []
+
+    // if (todosJSON !== null) {
+    //     return JSON.parse(todosJSON)
+    // } else {
+    //     return []
+    // }
 }
 
 // Save todos to localStorage 
